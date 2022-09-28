a13f5404696ec519e9fd10c9d39445546d5ce4ab
traceyhemp12 <twright48@email.davenport.edu>
2022-09-27 21:45:08 -0400

    adding loginview

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -2,8 +2,21 @@
 //  LoginView.swift
 //  Bankey
 //
-//  Created by Tracey (Mommy) on 9/14/22.
+//  Created by Tracey on 9/14/22.
 //  Copyright © 2022 TEKKStudios, LLC. All rights reserved.
 //
-
 import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+    }
+    
+    
+    
+}
