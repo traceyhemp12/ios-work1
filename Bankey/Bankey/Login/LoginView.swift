5f1f854fbb3b150d3c40efa4d96e232b68920cf2
traceyhemp12 <twright48@email.davenport.edu>
2022-09-27 21:56:25 -0400

    adding rest of loginview

--- Bankey/Bankey/Login/LoginView.swift
+++ Bankey/Bankey/Login/LoginView.swift
@@ -17,6 +17,22 @@ class LoginView: UIView {
         layout()
     }
     
+    required init?(coder:NSCoder){
+        fatalError("init(coder: ) has not been implemented")
+    }
     
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+}
+
+extension LoginView{
     
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout (){
+        
+    }
 }
