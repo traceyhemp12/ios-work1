0fd4865c7a559f9aa1528449d2cd8715e5f14a68
traceyhemp12 <twright48@email.davenport.edu>
2022-09-29 20:40:45 -0400

    centered part of login view

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -4,17 +4,33 @@
 //
 //  Created by Tracey (Mommy) on 9/12/22.
 //  Copyright © 2022 TEKKStudios, LLC. All rights reserved.
-//
 
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension LoginViewController {
+    private func style(){
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+            
+    private func layout(){
+        view.addSubview(loginView)
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter:view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
 }
 
